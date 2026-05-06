d80ac4676224ee01a680d8d81509697fbc70f6e5
Sri Mourya <srimouryav@emailcom>
2026-05-06 14:23:29 +0530

    connected frontend to live backend

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Get current user
 export const getUser = async () => {
